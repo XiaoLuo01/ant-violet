f98d2f7629cbc3608820010811022a5489250327
XiaoLuo01 <1428620591@qq.com>
2018-12-09 21:09:36 +0800

    测试用例

---
+++ tests/unit/button.spec.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import Button from '@/basic/button/button.vue'
+
+describe('Button 组件', () => {
+  it('is ok', () => {
+    expect(1).to.eq(1)
+  })
+})
