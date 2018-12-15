5e12a27ddb789e645b991f8ba1284e926727037d
XiaoLuo01 <1428620591@qq.com>
2018-12-15 23:36:02 +0800

    文档官网侧边栏样式修改

---
+++ docs/.vuepress/components/HomeLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+      111
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- docs/README.md
+++ docs/README.md
@@ -1,5 +1,8 @@
+<!-- ---
+layout: HomeLayout
+--- -->
+
 
 # Ant-violet 官网
 
 Ant-violet 是一个好用的 UI 框架, 提供了 ...... 等常用组件, 适合移动端和 PC 端使用.
-
