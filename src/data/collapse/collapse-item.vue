b5a8bb0ebd4f9065fc02e799372686e517a09f9e
XiaoLuo01 <1428620591@qq.com>
2019-02-24 22:51:09 +0800

    基本完成 collapse 功能

--- src/data/collapse/collapse-item.vue
+++ src/data/collapse/collapse-item.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="v-collapse-item">
-    <div class="title" @click="toggle">
+    <div class="title" @click="toggle" :data-name="name">
       {{title}}
     </div>
     <div class="content" v-if="open">
@@ -17,15 +17,36 @@ export default {
       open: false
     }
   },
+  inject: ['eventBus'],
   props: {
     title: {
       type: String,
       required: true
+    },
+    name: {
+      type: String,
+      required: true
     }
+  },
+  mounted() {
+    this.eventBus && this.eventBus.$on('update:selected', (names) => {
+      // 传过来的 names 进行判断, 有则展开, 没有则不展开
+      if(names.indexOf(this.name) < 0) {
+        this.open = false
+      } else {
+        this.open = true
+      }
+    })
   },
   methods: {
     toggle() {
-      this.open = !this.open
+      if(this.open) {
+        // 如果当前是展开的状态, 点击的时候就触发移除的函数
+        this.eventBus && this.eventBus.$emit('update:removeSelected', this.name)
+      } else {
+        // 如果当前是关闭的状态, 点击的时候就触发添加的函数
+        this.eventBus && this.eventBus.$emit('update:addSelected', this.name)
+      }
     }
   }
 }
--- src/data/collapse/collapse.vue
+++ src/data/collapse/collapse.vue
@@ -5,13 +5,59 @@
 </template>
 
 <script>
+import Vue from 'vue'
 export default {
   name: 'vCollapse',
-  data() {
-    return {
-
+  props: {
+    accordion: {
+      type: Boolean,
+      default: false
+    },
+    selected: {
+      type: Array
     }
   },
+  data() {
+    return {
+      eventBus: new Vue()
+    }
+  },
+  provide() {
+    return {
+      eventBus: this.eventBus
+    }
+  },
+  mounted() {
+    // toggle 的时候触发子组件的 显示和隐藏 事件
+    this.eventBus.$emit('update:selected', this.selected)
+
+    // 在父组件监听 添加selected 的事情, 在子组件 toggle 的时候触发事件
+    this.eventBus.$on('update:addSelected', (name) => {
+      let selectedCopy = JSON.parse(JSON.stringify(this.selected))
+      // 是否是手风琴效果, 只展开一个
+      if(this.accordion) {
+        selectedCopy = [name]
+      } else {
+        selectedCopy.push(name)
+      }
+      // 修改selected 数组之后, 触发 更新selected 事件, 通知子组件
+      this.eventBus.$emit('update:selected', selectedCopy)
+      // 同时也要通知外界 
+      this.$emit('update:selected', selectedCopy)
+    })
+
+    // 在父组件监听 移除selected 的事情, 在子组件 toggle 的时候触发事件
+    this.eventBus.$on('update:removeSelected', (name) => {
+      let selectedCopy = JSON.parse(JSON.stringify(this.selected))
+      // 关闭的时候移除当前 selected
+      let index = selectedCopy.indexOf(name)
+      selectedCopy.splice(index, 1)
+      // 修改selected 数组之后, 触发 更新selected 事件, 通知子组件
+      this.eventBus.$emit('update:selected', selectedCopy)
+      // 同时也要通知外界 
+      this.$emit('update:selected', selectedCopy)
+    })
+  }
 }
 </script>
 
