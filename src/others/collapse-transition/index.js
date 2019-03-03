a31f491910424cbca1cd6f613ba5876143b3bbf3
XiaoLuo01 <1428620591@qq.com>
2019-03-03 22:15:42 +0800

    完成 折叠展开的 小动画组件

---
+++ src/others/collapse-transition/index.js
@@ -0,0 +1,6 @@
+
+import CollapseTransition from './collapse-transition.vue'
+
+CollapseTransition.install = Vue => Vue.component(CollapseTransition.name, CollapseTransition)
+
+export default CollapseTransition
