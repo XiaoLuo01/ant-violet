f1edda794fae2d80e1603eb3a6a02e26b0036541
XiaoLuo01 <1428620591@qq.com>
2019-03-07 23:36:11 +0800

    完成 message 基本功能

--- src/notice/message/index.js
+++ src/notice/message/index.js
@@ -1,7 +1,15 @@
 import messageComponent from './message.vue'
 
 const Message = {
-
+  install(Vue, options) {
+    Vue.prototype.$message = function (msg) {
+      let Constructor = Vue.extend(messageComponent)
+      let message = new Constructor()
+      message.$slots.default = [msg]
+      message.$mount()
+      document.body.appendChild(message.$el)
+    }
+  }
 }
 
 export default Message
--- src/notice/message/message.vue
+++ src/notice/message/message.vue
@@ -1,20 +1,103 @@
 <template>
   <div class="v-message">
-
+    <v-icon name='v-info' class="v-info"></v-icon>
+    <slot></slot>
+    <div @click="onClickClose">
+      <v-icon name='v-close' class="v-close"></v-icon>
+    </div>
   </div>
 </template>
 
 <script>
+import Icon from './../../basic/icon/icon'
 export default {
   name: 'vMessage',
+  props: {
+    autoClose: {
+      type: Boolean,
+      default: false
+    },
+    autoCloseDelay: {
+      type: Number,
+      default: 3
+    },
+    closeButton: {
+      type: Boolean,
+      default: true
+    },
+    enableHtml: {
+      type: Boolean,
+      default: false
+    },
+    position: {
+      type: String,
+      default: "top",
+      validator(value) {
+        return ["top", "bottom", "middle"].indexOf(value) >= 0
+      }
+    }
+  },
   data() {
     return {
 
     }
   },
+  components: {
+    'v-icon': Icon
+  },
+  mounted() {
+    this.execAutoClose()
+  },
+  methods: {
+    execAutoClose() {
+      if (this.autoClose) {
+        setTimeout(() => {
+          this.close()
+        }, this.autoCloseDelay * 1000)
+      }
+    },
+    close() {
+      this.$el.remove()
+      this.$destroy()
+    },
+    onClickClose() {
+      this.close()
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
-
+.v-message {
+  min-width: 380px;
+  border-radius: $border-radius;
+  position: fixed;
+  left: 50%;
+  top: 20px;
+  transform: translateX(-50%);
+  background: #edf2fc;
+  display: flex;
+  align-items: center;
+  padding: 15px 15px 15px 20px;
+  z-index: 100;
+  color: #909399;
+  font-size: 14px;
+  .v-info {
+    width: 16px;
+    height: 16px;
+    fill: #909399;
+    margin: 0;
+    padding-right: 10px;
+  }
+  .v-close {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    right: 0;
+    width: 16px;
+    height: 16px;
+    fill: #c0c4cc;
+    cursor: pointer;
+  }
+}
 </style>
