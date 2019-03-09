b6d0bd4681a3a471137c485a89da43cae1ccc277
XiaoLuo01 <1428620591@qq.com>
2019-03-09 22:25:53 +0800

    完成 hover 显示内容

--- src/others/popover/popover.vue
+++ src/others/popover/popover.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="v-popover" ref="popover" @click="onClick">
+  <div class="v-popover" ref="popover">
     <div ref="contentWrapper" class="content-wrapper" v-if="visible" :class="{[`position-${position}`]: true}">
-      <slot name="content"></slot>
+      <slot name="content" :close="close"></slot>
     </div>
     <span ref="triggerWrapper" style="display: inline-block">
       <slot></slot>
@@ -34,10 +34,20 @@ export default {
     }
   },
   mounted() {
-
+    if(this.trigger === 'click') {
+      this.$refs.popover.addEventListener('click', this.onClick)
+    } else {
+      this.$refs.popover.addEventListener('mouseenter', this.open)
+      this.$refs.popover.addEventListener('mouseleave', this.close)
+    }
   },
   destroyed() {
-
+    if(this.trigger === 'click') {
+        this.$refs.popover.removeEventListener('click', this.onClick)
+    } else {
+        this.$refs.popover.removeEventListener('mouseenter', this.open)
+        this.$refs.popover.removeEventListener('mouseleave', this.close)
+    }
   },
   methods: {
     positionContent() {
