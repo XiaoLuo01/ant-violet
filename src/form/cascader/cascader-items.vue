30bcea7abed4a24ff6721f67a9c0a0925f0f5fbf
XiaoLuo01 <1428620591@qq.com>
2019-03-17 22:14:27 +0800

    给 cascader 组件添加样式和动画

---
+++ src/form/cascader/cascader-items.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="v-cascader-items">
+    <div class="left">
+      
+    </div>
+    <div class="right">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import vIcon from "../../basic/icon/icon";
+export default {
+  name: 'vCascaderItems',
+  props: {
+    items: {
+      type: Array
+    },
+    height: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      leftSelected: null
+    }
+  },
+  components: {
+    Icon
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.v-cascader-items {
+
+}
+</style>
--- src/form/cascader/cascader.vue
+++ src/form/cascader/cascader.vue
@@ -1,25 +1,73 @@
 <template>
   <div class="v-cascader">
-    <div class="trigger">
-      <slot></slot>
+    <div class="trigger" :class="{ active: popoverVisible }" @click="popoverVisible = !popoverVisible">
+      <v-icon name='v-arrow-right' class="v-icon"></v-icon>
     </div>
-    <div class="popover">
-      
-    </div>
+    <v-collaspe-transition class="popover-wrapper" :visible="popoverVisible">
+      <cascader-items></cascader-items>
+    </v-collaspe-transition>
   </div>
 </template>
 
 <script>
+import vIcon from "../../basic/icon/icon";
+import CascaderItems from './cascader-items';
+import CollaspeTransition from "../../others/collapse-transition/collapse-transition";
 export default {
   name: 'vCascader',
   data() {
     return {
-
+      popoverVisible: false,
+      isActive: false,
     }
   },
+  components: { 
+    'v-collaspe-transition' : CollaspeTransition, 
+    'v-icon': vIcon,
+    CascaderItems
+  },
+  methods: {
+  }
 }
 </script>
 
 <style scoped lang="scss">
-
+.v-cascader {
+  position: relative;
+  display: inline-block;
+  > .trigger {
+    height: 32px;
+    width: 200px;
+    padding: 0 1em;
+    border: 1px solid #dcdfe6;
+    border-radius: $border-radius;
+    cursor: pointer;
+    display: inline-flex;
+    align-items: center;
+    &.active {
+      border-color: #409eff;
+      > .v-icon {
+        transform: rotateZ(90deg);
+      }
+    }
+    > .v-icon {
+      position: absolute;
+      right: 0;
+      width: 12px;
+      height: 12px;
+      margin-right: 10px;
+      fill: #c0c4cc;
+      transition: all .3s;
+    }
+  }
+  > .popover-wrapper {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    z-index: 99;
+    background: #fff;
+    display: flex;
+    @extend .box-shadow;
+  }
+}
 </style>
