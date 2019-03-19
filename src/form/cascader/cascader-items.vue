a0e2b68819aec446cc4f81e5debd6b0bfeccd0e7
XiaoLuo01 <1428620591@qq.com>
2019-03-19 23:12:40 +0800

    解决右边不更新的 bug

--- src/form/cascader/cascader-items.vue
+++ src/form/cascader/cascader-items.vue
@@ -42,11 +42,12 @@ export default {
   },
   computed: {
     rightItems() {
-      let currentSelected = this.selected[this.level];
-      if(currentSelected && currentSelected.children) {
-        return currentSelected.children
-      } else {
-        return null
+      // 通过 this.items 去查找更新
+      if ( this.selected[this.level] ) {
+        let selected = this.items.filter(item => item.name === this.selected[this.level].name)
+        if(selected && selected[0].children && selected[0].children.length > 0) {
+          return selected[0].children
+        }
       }
     }
   },
