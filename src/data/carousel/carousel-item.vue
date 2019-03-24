ad122402050d1e7f2d089eecac273560bea0ca0e
XiaoLuo01 <1428620591@qq.com>
2019-03-24 21:45:29 +0800

    创建 carousel 组件

---
+++ src/data/carousel/carousel-item.vue
@@ -0,0 +1,42 @@
+<template>
+  <transition name="carousel">
+    <div class="v-carousel-item" v-if="visible">
+      <slot></slot>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'vCarousel',
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      visible: false,
+      selected: undefined
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.carousel-leave-active {
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+.carousel-enter-active, .carousel-leave-active {
+  transition: all 1s;
+}
+.carousel-enter {
+  transform: translateX(100%);
+}
+.carousel-leave-to {
+  transform: translateX(-100%);
+}
+</style>
---
+++ src/data/carousel/carousel.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="v-carousel">
+    <div class="v-carousel-window">
+      <div class="v-carousel-wrapper">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vCarousel',
+  props: {
+    selected: {
+      type: String
+    }
+  },
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+    let first = this.$children[0]
+    let second = this.$children[1]
+    first.visible = true
+    setTimeout(() => {
+      first.visible = false
+      second.visible = true
+    }, 2000)
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.v-carousel {
+  display: inline-block;
+  overflow: hidden;
+  &-wrapper {
+    position: relative;
+  }
+}
+</style>
