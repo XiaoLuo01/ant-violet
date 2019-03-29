43661b85d26e192d4a8d80c3d19f3b3952c1af07
XiaoLuo01 <1428620591@qq.com>
2019-03-29 23:43:48 +0800

    支持移动设备滑动

--- src/data/carousel/carousel.vue
+++ src/data/carousel/carousel.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="v-carousel" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
+  <div class="v-carousel" 
+  @mouseenter="onMouseEnter" 
+  @mouseleave="onMouseLeave"
+  @touchstart="onTouchStart"
+  @touchmove="onTouchMove"
+  @touchend="onTouchEnd">
     <div class="v-carousel-window">
       <div class="v-carousel-wrapper">
         <slot></slot>
       </div>
     </div>
     <div class="v-carousel-dots">
-      <span v-for="n in childrenLength" :key="'index_' + n" :class="{active : selectedIndex === n-1}" @click="select(n-1)">
-        {{n}}
-      </span>
+      <span v-for="n in childrenLength" :key="'index_' + n" :class="{active : selectedIndex === n-1}" @click="select(n-1)"></span>
     </div>
   </div>
 </template>
@@ -29,12 +32,14 @@ export default {
     return {
       childrenLength: 0,
       lastSelectedIndex: undefined,
-      timerId: undefined
+      timerId: undefined,
+      startTouch: undefined
     }
   },
   computed: {
     selectedIndex() {
-      return this.names.indexOf(this.selected) || 0
+      let index = this.names.indexOf(this.selected)
+      return index === -1 ? 0:index
     },
     names() {
       return this.$children.map(vm => vm.name)
@@ -55,8 +60,6 @@ export default {
       let run = () => {
         let index = this.names.indexOf(this.getSelected())
         let newIndex = index + 1
-        if (newIndex === -1) { newIndex = this.names.length + 1 }
-        if (newIndex === this.names.length) { newIndex = 0 }
         this.select(newIndex) // 告诉外界选中 newIndex
         this.timerId = setTimeout(run, 2000)
       }
@@ -76,14 +79,42 @@ export default {
       let first = this.$children[0]
       return this.selected || first.name
     },
+    onTouchStart(e) {
+      this.pause()
+      if (e.touches.length > 1) { return }
+      this.startTouch = e.touches[0]
+    },
+    onTouchMove(e) {
+
+    },
+    onTouchEnd(e) {
+      let endTouch = e.changedTouches[0]
+      let {clientX:x1, clientY:y1} = this.startTouch
+      let {clientX:x2, clientY:y2} = endTouch
+
+      let distance = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2))
+      let deltaY = Math.abs(y2-y1)
+      let rate = distance / deltaY
+      if (rate > 2) {
+        if (x2 < x1) {
+          this.select(this.selectedIndex + 1)
+        } else {
+          this.select(this.selectedIndex - 1)
+        }
+      }
+
+      this.playAutomatically()
+    },
     updatedChildren() {
       this.$children.forEach(vm => {
         let reverse = this.selectedIndex > this.lastSelectedIndex ? false : true
-        if (this.lastSelectedIndex === this.$children.length -1 && this.selectedIndex === 0) {
-          reverse = false
-        }
-        if (this.lastSelectedIndex === 0 && this.selectedIndex === this.$children.length - 1 ) {
-          reverse = true
+        if (this.timerId) {
+            if (this.lastSelectedIndex === this.$children.length -1 && this.selectedIndex === 0) {
+            reverse = false
+          }
+          if (this.lastSelectedIndex === 0 && this.selectedIndex === this.$children.length - 1 ) {
+            reverse = true
+          }
         }
         vm.reverse = reverse
         this.$nextTick(() => {
@@ -91,9 +122,11 @@ export default {
         })
       })
     },
-    select(index) {
+    select(newIndex) {
+      if (newIndex === -1) { newIndex = this.names.length - 1 }
+      if (newIndex === this.names.length) { newIndex = 0 }
       this.lastSelectedIndex = this.selectedIndex
-      this.$emit('update:selected', this.names[index])
+      this.$emit('update:selected', this.names[newIndex])
     }
   }
 }
@@ -101,6 +134,7 @@ export default {
 
 <style scoped lang="scss">
 .v-carousel {
+  position: relative;
   &-window {
     overflow: hidden;
   }
@@ -108,9 +142,22 @@ export default {
     position: relative;
   }
   &-dots {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: 10px;
     > span {
+      width: 10px;
+      height: 10px;
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 50%;
+      background: #fff;
+      margin: 0 4px;
+      cursor: pointer;
       &.active {
-        background: red;
+        background: #99a9bf;
       }
     }
   }
