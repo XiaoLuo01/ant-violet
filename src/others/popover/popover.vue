d78a21d0c655301b5d5f851aa7ab503c4eae87c4
XiaoLuo01 <1428620591@qq.com>
2019-03-31 10:50:44 +0800

    修改 popover 组件的销毁钩子

--- src/others/popover/popover.vue
+++ src/others/popover/popover.vue
@@ -41,7 +41,7 @@ export default {
       this.$refs.popover.addEventListener('mouseleave', this.close)
     }
   },
-  destroyed() {
+  beforeDestroy() {
     if(this.trigger === 'click') {
         this.$refs.popover.removeEventListener('click', this.onClick)
     } else {
