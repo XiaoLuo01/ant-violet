4566330dae4e759503a41e18c1e757ea9383362b
XiaoLuo01 <1428620591@qq.com>
2019-04-28 22:36:10 +0800

    完成大部分样式

--- src/navigation/pagination/pagination.vue
+++ src/navigation/pagination/pagination.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="v-pagination">
-    <span v-for="(page, i) in pages" :key="i" class="v-pagination-item" :class="{active: page === currentPage, separator: page === '•••'}">
-      {{page}}
+    <span class="v-pagination-item" :class="{disabled:currentPage === 1}">
+      <v-icon name="v-arrow-left"></v-icon>
+    </span>
+    <template v-for="(page, i) in pages">
+      <template v-if="page === currentPage">
+        <span class="v-pagination-item current" :key="i">{{page}}</span>
+      </template>
+      <template v-else-if="page === '•••'">
+        <span class="v-pagination-item separator" :key="i">{{page}}</span>
+      </template>
+      <template v-else>
+        <span class="v-pagination-item" :key="i">{{page}}</span>
+      </template>
+    </template>
+    <span class="v-pagination-item" :class="{disabled:currentPage === totalPage}">
+      <v-icon name="v-arrow-right"></v-icon>
     </span>
   </div>
 </template>
 
 <script>
+import vIcon from "../../basic/icon/icon";
 export default {
   name: 'vPagination',
   props: {
@@ -23,17 +38,22 @@ export default {
       default: true
     }
   },
+  components: {
+    'v-icon': vIcon 
+  },
   data() {
-    let pages = [1, this.totalPage, this.currentPage, this.currentPage -1, this.currentPage -2, this.currentPage + 1, this.currentPage +2]
-    let u = unique(pages.sort((a, b) => a - b))
-    let u2 = u.reduce((prev, current, index, array) => {
+    let pages = unique([1, this.totalPage, this.currentPage, this.currentPage -1, this.currentPage -2, 
+    this.currentPage + 1, this.currentPage +2])
+    .filter((n) => n >= 1 && n <= this.totalPage )
+    .sort((a, b) => a - b)
+    .reduce((prev, current, index, array) => {
       prev.push(current)
-      array[index + 1] && array[index + 1] - array[index] > 1 && prev.push('•••')
+      array[index + 1]  !== undefined && array[index + 1] - array[index] > 1 && prev.push('•••')
       return prev
     }, [])
 
     return {
-      pages: u2
+      pages
     }
   },
 }
@@ -50,20 +70,23 @@ function unique(array) {
 <style scoped lang="scss">
 .v-pagination {
   &-item {
-    display: inline-block;
+    display: inline-flex;
     min-width: 32px;
     height: 32px;
     margin-right: 8px;
-    line-height: 30px;
-    text-align: center;
+    justify-content: center;
+    align-items: center;
     vertical-align: middle;
     background-color: #fff;
     border: 1px solid #d9d9d9;
     border-radius: 4px;
     cursor: pointer;
-    &.active, &:hover {
+    &.current, &:hover {
       border-color: #40a9ff;
       color: #40a9ff;
+      .v-icon {
+        fill:#40a9ff;
+      }
     }
     &.separator {
       color: rgba(0,0,0,0.25);
@@ -71,6 +94,19 @@ function unique(array) {
       border: none;
       text-indent: .13em;
       cursor: default;
+    }
+    &.disabled {
+      cursor: not-allowed;
+      border-color: #d9d9d9;
+      .v-icon {
+        fill: rgba(0,0,0,0.25);
+      }
+    }
+    .v-icon {
+      margin: 0;
+      fill: #666;
+      width: 14px;
+      height: 14px;
     }
   }
 }
