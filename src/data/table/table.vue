863c816794301a92d5256bffc91c24bbd280f36b
XiaoLuo01 <1428620591@qq.com>
2019-05-21 23:39:49 +0800

    完成 table 可展开的功能

--- src/data/table/table.vue
+++ src/data/table/table.vue
@@ -4,13 +4,14 @@
       <table class="v-table-wrapper" :class="{bordered, compact, striped}" ref="table">
         <thead class="v-table-thead">
           <tr>
-            <th class="v-table-selection-column" style="{width: '50px'}">
+            <th class="v-table-selection-column" v-if="checkable">
               <input type="checkbox" class="v-checkbox-input" @change="onChangeAllItems" ref="allCheck" :checked="areAllItemsSelected">
             </th>
+            <th class="v-table-selection-column" v-if="expandField"></th>
             <th v-for="(column, i) in columns" :key="'th_'+i" class=" is-sortable" @click="changeOrderBy(column.field)" :style="{width: column.width + 'px'}">
               <div>
                 {{column.text}}
-                <span v-if="column.field in orderBy" class="caret-wrapper" >
+                <span v-if="orderBy && column.field in orderBy" class="caret-wrapper" >
                   <v-icon name='v-caret-top' class="v-icon-top" :class="{active: orderBy[column.field] === 'asc'}"></v-icon>
                   <v-icon name='v-caret-bottom' class="v-icon-bottom" :class="{active: orderBy[column.field] === 'desc'}"></v-icon>
                 </span>
@@ -19,14 +20,25 @@
           </tr>
         </thead>
         <tbody class="v-table-tbody">
-          <tr v-for="(item, index) in dataSource" :key="'tr_'+index">
-            <td class="v-table-selection-column"  style="{width: '50px'}">
-              <input type="checkbox" class="v-checkbox-input" @change="onChangeItem($event, item, index)" :checked="inSelectedItem(item)">
-            </td>
-            <template v-for="(column, k) in columns">
-              <td  :key="'td_'+k" :style="{width: column.width + 'px'}">{{item[column.field]}}</td>
-            </template>
-          </tr>
+          <template v-for="(item, index) in dataSource">
+            <tr :key="'tr_'+index">
+              <td class="v-table-selection-column" v-if="checkable">
+                <input type="checkbox" class="v-checkbox-input" @change="onChangeItem($event, item, index)" :checked="inSelectedItem(item)">
+              </td>
+              <td class="v-table-selection-column" v-if="expandField">
+                <v-icon v-if="item[expandField]" name="v-arrow-right" @click="expandItem(item.id)" :class="{expand: inExpandIds(item.id)}"></v-icon>
+              </td>
+              <template v-for="(column, k) in columns">
+                <td  :key="'td_'+k" :style="{width: column.width + 'px'}">{{item[column.field]}}</td>
+              </template>
+            </tr>
+            <tr v-if="item[expandField] && inExpandIds(item.id)" :key="'tr2_'+index">
+              <td ></td>
+              <td :colspan="columns.length + expandedCellColspan">
+                {{item[expandField]}}
+              </td>
+            </tr>
+          </template>
         </tbody>
       </table>
     </div>
@@ -57,6 +69,9 @@ export default {
         return !(array.filter(item => item.id === undefined).length > 0)
       }
     },
+    expandField: {
+      type: String
+    },
     loading: {
       type: Boolean,
       default: false
@@ -79,11 +94,15 @@ export default {
     },
     height: {
       type: Number
+    },
+    checkable: {
+      type: Boolean,
+      default: false
     }
   },
   data() {
     return {
-
+      expandedIds: []
     }
   },
   components: {
@@ -106,6 +125,12 @@ export default {
           }
         }
       return equal
+    },
+    expandedCellColspan() {
+      let result = 0
+      if (this.checkable) { result += 1} 
+      if (this.expandField) { result += 1} 
+      return result
     }
   },
   mounted() {
@@ -124,7 +149,20 @@ export default {
     this.table2.remove()
   },
   methods: {
+    inExpandIds(id) {
+      return this.expandedIds.indexOf(id) >= 0
+    },
+    expandItem(id){
+      if (this.inExpandIds(id)) {
+        this.expandedIds.splice(this.expandedIds.indexOf(id), 1)
+      } else {
+        this.expandedIds.push(id)
+      }
+    },
     changeOrderBy(key) {
+      if (!this.orderBy) {
+        return
+      }
       let copy = JSON.parse(JSON.stringify(this.orderBy))
       let oldValue = copy[key]
       if (oldValue === 'asc') {
@@ -177,8 +215,20 @@ export default {
     display: table;
     border-collapse: collapse;
     margin: 0;
-    .v-table-selection-column {
+    tr .v-table-selection-column {
       width: 28px;
+      padding: 16px 10px;
+      text-align: center;
+      // position: relative;
+      .v-icon {
+        width: 16px;
+        height: 16px;
+        margin: 0;
+        transition: all .3s;
+        &.expand {
+          transform: rotate(90deg);
+        }
+      }
     }
     &.bordered {
       border: 1px solid #e8e8e8;
@@ -194,9 +244,9 @@ export default {
       }
     }
     .v-table-thead {
-        > tr {
+      tr {
         background: #fafafa;
-        > th {
+        th {
           color: rgba(0,0,0,0.75);
           text-align: left;
           border-bottom: 1px solid #e8e8e8;
